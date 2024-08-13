b175da471cbb3945c8ed0dad0b1835f6c0686de3
samirasal <samirasalifu5522@gmail.com>
2024-08-13 12:02:43 -0400

    Reduce method
    Made use of the reduce method to find the total number of contributions and money raised. Then found the total number of games using simple methods like the length and innerHTML

--- index.js
+++ index.js
@@ -47,7 +47,7 @@ function addGamesToPage(games) {
         gameCard.innerHTML = `
         <h3>${game.name}</h3>
         <p>${game.description}</p>
-        <img src = "${game-img}" alt = "${game.name}"/>`;
+        <img src = "${game.img}" alt = "${game.name}" />`;
 
         // append the game to the games-container
         gamesContainer.appendChild(gameCard);
@@ -72,21 +72,30 @@ addGamesToPage(GAMES_JSON)
 const contributionsCard = document.getElementById("num-contributions");
 
 // use reduce() to count the number of total contributions by summing the backers
-
+const totalContributions = GAMES_JSON.reduce( (acc, game) => {
+    return acc + game.backers;
+  }, 0);
+  console.log(totalContributions)
 
 // set the inner HTML using a template literal and toLocaleString to get a number with commas
-
+contributionsCard.innerHTML = `${totalContributions.toLocaleString()}`;
 
 // grab the amount raised card, then use reduce() to find the total amount raised
 const raisedCard = document.getElementById("total-raised");
+const totalAmount = GAMES_JSON.reduce( (acc, game) => {
+    return acc + game.pledged;
+  }, 0);
+ 
 
 // set inner HTML using template literal
-
+raisedCard.innerHTML = `$${totalAmount.toLocaleString()}`;
 
 // grab number of games card and set its inner HTML
 const gamesCard = document.getElementById("num-games");
 
+const totalGames = GAMES_JSON.length;
 
+gamesCard.innerHTML = `${totalGames}`;
 /*************************************************************************************
  * Challenge 5: Add functions to filter the funded and unfunded games
  * total number of contributions, amount donated, and number of games on the site.
