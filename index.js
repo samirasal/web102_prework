07bfc2c8f74652e96792e818946c755e95a0f3f0
samirasal <samirasalifu5522@gmail.com>
2024-08-13 09:56:59 -0400

    worked on .js and .css files
    added new attributes to the stats-container class and added a hover effect for the CSS file and for the JS file, created a function that adds all data from the games array to the page

--- index.js
+++ index.js
@@ -5,6 +5,7 @@
 */
 
 // import the JSON data about the crowd funded games from the games.js file
+import games from './games.js';
 import GAMES_DATA from './games.js';
 
 // create a list of objects to store the data about the games using JSON.parse
@@ -27,27 +28,37 @@ const gamesContainer = document.getElementById("games-container");
 
 // create a function that adds all data from the games array to the page
 function addGamesToPage(games) {
-
+ 
     // loop over each item in the data
+    for (let i = 0; i < games.length; i++ ){
 
-
+        const game = games[i];
+    
         // create a new div element, which will become the game card
-
+        let gameCard = document.createElement("div");
 
         // add the class game-card to the list
-
+        gameCard.classList.add("game-card")
 
         // set the inner HTML using a template literal to display some info 
         // about each game
         // TIP: if your images are not displaying, make sure there is space
         // between the end of the src attribute and the end of the tag ("/>")
-
+        gameCard.innerHTML = `
+        <h3>${game.name}</h3>
+        <p>${game.description}</p>
+        <img src = "${game-img}" alt = "${game.name}"/>`;
 
         // append the game to the games-container
+        gamesContainer.appendChild(gameCard);
+
+}
 
 }
 
 // call the function we just defined using the correct variable
+
+addGamesToPage(GAMES_JSON)
 // later, we'll call this function using a different list of games
 
 
